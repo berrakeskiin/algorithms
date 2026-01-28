0b70969106b59aede99842510952af1510011024
berra gul <berrakeskin@berras-MacBook-Air.local>
2026-01-28 22:00:33 +0100

    Task 2 - find divisors

---
+++ task2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<long> findDivisors(long number);
+
+int main() {
+    long number;
+    cout << "Enter an integer number:";
+    cin >> number;
+
+    vector<long> result = findDivisors(number);
+
+    for (int i = 0; i < result.size(); i++) {
+        cout << result[i] << " ";
+    }
+}
+
+vector<long> findDivisors(long number) {
+    vector<long> divisors;
+
+    for (long i = 1; i <= number; i++) {
+        if (number % i == 0) {
+            divisors.push_back(i);
+        }
+    }
+
+    return divisors;
+}
